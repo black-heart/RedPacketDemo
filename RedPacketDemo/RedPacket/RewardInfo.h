19436e267af0db8707411db086fea11182f3a862
black-heart <mrqingyu@gmail.com>
2016-08-16 17:42:32 +0800

    增加截屏

--- RedPacketDemo/RedPacket/RewardInfo.h
+++ RedPacketDemo/RedPacket/RewardInfo.h
@@ -8,7 +8,7 @@
 
 #import <Foundation/Foundation.h>
 
-@interface RewardInfo : NSObject<NSCoding>
+@interface RewardInfo : NSObject
 @property (nonatomic, assign) float     money;          //金额
 @property (nonatomic, assign) NSInteger rewardStatus;   //红包状态  0.未领取 1.已领取 2.已过期 3.红包已领完
 @property (nonatomic, copy  ) NSString  *rewardContent; //红包打开后提示内容
--- RedPacketDemo/RedPacket/RewardInfo.m
+++ RedPacketDemo/RedPacket/RewardInfo.m
@@ -9,21 +9,21 @@
 #import "RewardInfo.h"
 
 @implementation RewardInfo
-- (void)encodeWithCoder:(NSCoder *)aCoder {
-    [aCoder encodeFloat:self.money forKey:@"money"];
-    [aCoder encodeInteger:self.rewardStatus forKey:@"rewardStatus"];
-    [aCoder encodeObject:self.rewardContent forKey:@"rewardContent"];
-    [aCoder encodeObject:self.rewardName forKey:@"rewardName"];
-}
-
-- (id)initWithCoder:(NSCoder *)aDecoder {
-    if (self = [super init]) {
-        self.money = [aDecoder decodeFloatForKey:@"money"];
-        self.rewardStatus = [aDecoder decodeIntegerForKey:@"rewardStatus"];
-        self.rewardContent = [aDecoder decodeObjectForKey:@"rewardContent"];
-        self.rewardName = [aDecoder decodeObjectForKey:@"rewardName"];
-    }
-    return self;
-}
+//- (void)encodeWithCoder:(NSCoder *)aCoder {
+//    [aCoder encodeFloat:self.money forKey:@"money"];
+//    [aCoder encodeInteger:self.rewardStatus forKey:@"rewardStatus"];
+//    [aCoder encodeObject:self.rewardContent forKey:@"rewardContent"];
+//    [aCoder encodeObject:self.rewardName forKey:@"rewardName"];
+//}
+//
+//- (id)initWithCoder:(NSCoder *)aDecoder {
+//    if (self = [super init]) {
+//        self.money = [aDecoder decodeFloatForKey:@"money"];
+//        self.rewardStatus = [aDecoder decodeIntegerForKey:@"rewardStatus"];
+//        self.rewardContent = [aDecoder decodeObjectForKey:@"rewardContent"];
+//        self.rewardName = [aDecoder decodeObjectForKey:@"rewardName"];
+//    }
+//    return self;
+//}
 
 @end
